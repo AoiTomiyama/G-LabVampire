b9ab31e6a2a9be59f3f8ea51c46c360a40f53724
AoiTomiyama <TadoO.o2209@gmail.com>
2024-09-14 11:05:42 +0900

    [fix] カメラの描画範囲を修正、自機の移動を仕様書に沿ったシステムに修正。

--- Assets/Member/Tomiyama/Scripts/PlayerMove.cs
+++ Assets/Member/Tomiyama/Scripts/PlayerMove.cs
@@ -6,14 +6,29 @@ public class PlayerMove : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
     private Rigidbody2D _rb;
+
+    int _h = 0;
+    int _v = 0;
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        _rb.velocity = _moveSpeed * new Vector2(h, v).normalized;
+        bool pressedUp = Input.GetButton("Up");
+        bool pressedDown = Input.GetButton("Down");
+        bool pressedLeft = Input.GetButton("Left");
+        bool pressedRight = Input.GetButton("Right");
+
+        _v = (pressedUp && !pressedDown) ? 1 : (!pressedUp && pressedDown) ? -1 : (pressedUp && pressedDown) ? _v : 0;
+        _h = (pressedRight && !pressedLeft) ? 1 : (!pressedRight && pressedLeft) ? -1 : (pressedLeft && pressedRight) ? _h : 0;
+
+        /*
+         * 上下、左右どちらかが押されている場合、入力に応じてそのまま値を入れる。
+         * 両方押されている場合、値を変更しない。
+         * 無入力の場合、0を入れる。
+         */
+
+        _rb.velocity = _moveSpeed * new Vector2(_h, _v);
     }
 }
